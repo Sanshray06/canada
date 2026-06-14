be6ca7245e6d7708e7ceecb9dff7c36d98f8b1b6
Sanshray Mittu <sanshraymittu6@gmail.com>
2026-06-14 22:22:27 +0530

    fourth commit

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+
+console.log(
+  "SUPABASE URL:",
+  process.env.NEXT_PUBLIC_SUPABASE_URL
+);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
